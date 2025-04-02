9e257d54d0f7ed04efe402989b243d6c37e504b3
DeepSeeDev <hyuk2000s@naver.com>
2025-04-02 11:05:18 +0900

    feat: react project started

--- src/components/CommunityIntro.jsx
+++ src/components/CommunityIntro.jsx
@@ -7,7 +7,7 @@ export default function CommunityIntro() {
       <h2 className="text-xl font-semibold">감정 공유 커뮤니티</h2>
       <p className="text-gray-500">
         비슷한 감정을 가진 사람들과 익명으로 소통하고 공감대를 형성해보세요
-      </p>
+      </p> 
     </div>
   )
 }
